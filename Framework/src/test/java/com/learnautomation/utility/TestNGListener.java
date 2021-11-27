e8280f5834045485dc9212c3ae5c5da5a4577050
Tom-spec113 <52246540+Tom-spec113@users.noreply.github.com>
2021-11-27 21:57:18 +0530

    updated print statements in TestNGListener.java file

--- Framework/src/test/java/com/learnautomation/utility/TestNGListener.java
+++ Framework/src/test/java/com/learnautomation/utility/TestNGListener.java
@@ -7,22 +7,22 @@ public class TestNGListener implements ITestListener {
 
 	public void onTestFailure(ITestResult result) {
 		
-		System.out.println("Test is failed. Test name is "+ result.getName());
+		System.out.println("Test failed. Test name is "+ result.getName());
 
 	}
 
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("Test is skipped. Test name is "+ result.getName());
+		System.out.println("Test skipped. Test name is "+ result.getName());
 
 	}
 
 	public void onTestStart(ITestResult result) {
-		System.out.println("Test is started. Test name is "+ result.getName());
+		System.out.println("Test started. Test name is "+ result.getName());
 
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("Test is success. Test name is "+ result.getName());
+		System.out.println("Test passed. Test name is "+ result.getName());
 
 	}
 }
